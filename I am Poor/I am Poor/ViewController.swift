e207cc14d467263eecf3b6e7aa31eed196e380d0
anujdutt9 <anuj.dutt9@gmail.com>
2019-05-11 19:15:29 -0400

    initial update

---
+++ I am Poor/I am Poor/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  I am Poor
+//
+//  Created by Anuj Dutt on 5/11/19.
+//  Copyright © 2019 Anuj Dutt. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
