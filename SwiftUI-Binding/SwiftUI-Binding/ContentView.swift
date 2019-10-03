73cb7326e923959c1e26fbf45f1b58a4e3997c63
Anuj Dutt <anuj.dutt9@gmail.com>
2019-10-03 15:21:34 -0400

    SwiftUI-Bindings

---
+++ SwiftUI-Binding/SwiftUI-Binding/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwiftUI-Binding
+//
+//  Created by Anuj Dutt on 10/1/19.
+//  Copyright © 2019 Anuj Dutt. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
